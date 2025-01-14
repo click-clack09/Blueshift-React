ae3e1f7e20b81e88e719dc5a10058fbaf6fb9b54
click-clack09 <77758597+click-clack09@users.noreply.github.com>
2025-01-14 17:52:20 -0600

    Testing page navigation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  createStaticNavigation,
+  useNavigation,
+} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button } from '@react-navigation/elements';
+import Demo from "./Demo";
 
 class App extends Component {
   render() {
     return (
-      <div className="App bg-yellow-600">
+      <div className="App">
         <header className="App-header">
           <div className="text-red-500">First React Page!!!</div>
           <img src={logo} className="App-logo" alt="logo" />
-          <a
+          <button onClick={() => navigation.navigate('Demo')}>
+        Go to Demo
+      </button>
+          {/* <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
         </header>
       </div>
     );
---
+++ src/Demo.jsx
@@ -0,0 +1,8 @@
+const Demo = () => {
+    return (
+        <div className="h-screen bg-white flex justify-center items-center">
+            <div className="text-3xl text-slate-700">My first page</div>
+        </div>
+    )
+};
+export default Demo;
